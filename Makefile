9fb909ee467afa5a565c575e492b882672f732b8
Elchanz3 <inutilidades639@gmail.com>
2025-08-24 20:12:30 -0300

    Revert "several fixes to build S20fe kernel"
    This reverts commit 359fa11a8a58f0c5bc4f15f7ffa05cee2461c95d.

--- Makefile
+++ Makefile
@@ -320,7 +320,7 @@ include scripts/subarch.include
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 ARCH            = arm64
-CROSS_COMPILE	?= /home/chanz22/tc/aarch64-linux-android-4.9-master/bin/aarch64-linux-android-
+CROSS_COMPILE	?= aarch64-linux-gnu
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
@@ -371,7 +371,7 @@ KBUILD_HOSTLDLIBS   := $(HOST_LFS_LIBS) $(HOSTLDLIBS)
 # Make variables (CC, etc...)
 AS		= $(CCACHE) llvm-as
 LD		= $(CCACHE) ld.lld
-CC              = $(CCACHE) /home/chanz22/tc/aosp-clang-r412851/bin/clang
+CC              = $(CCACHE) /home/chanz22/tc/puppy_clang/bin/clang
 CPP		= $(CC) -E
 AR		= $(CCACHE) llvm-ar
 NM		= llvm-nm
--- drivers/net/wireless/qualcomm/qca6390/qcacld-3.0/core/sap/src/sap_fsm.c
+++ drivers/net/wireless/qualcomm/qca6390/qcacld-3.0/core/sap/src/sap_fsm.c
@@ -58,14 +58,6 @@
 #include "wlan_policy_mgr_ucfg.h"
 #include "cfg_ucfg_api.h"
 
-#ifdef __KERNEL__
-    // Use kernel types
-    #include <linux/types.h>
-#else
-    // For user-space programs, include stdint.h
-    #include <stdint.h>
-#endif
-
 /*----------------------------------------------------------------------------
  * Preprocessor Definitions and Constants
  * -------------------------------------------------------------------------*/
@@ -1474,7 +1466,7 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 			return QDF_STATUS_E_INVAL;
 		}
 
-		bss_complete->status = (eSapStatus)(uintptr_t)context;
+		bss_complete->status = (eSapStatus) context;
 		bss_complete->staId = sap_ctx->sap_sta_id;
 
 		QDF_TRACE(QDF_MODULE_ID_SAP, QDF_TRACE_LEVEL_INFO_HIGH,
@@ -1493,16 +1485,16 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 	case eSAP_DFS_NO_AVAILABLE_CHANNEL:
 		sap_ap_event.sapHddEventCode = sap_hddevent;
 		sap_ap_event.sapevt.sapStopBssCompleteEvent.status =
-			(eSapStatus)(uintptr_t)context;
+			(eSapStatus) context;
 		break;
 	case eSAP_ACS_SCAN_SUCCESS_EVENT:
 		sap_handle_acs_scan_event(sap_ctx, &sap_ap_event,
-			(eSapStatus)(uintptr_t)context);
+			(eSapStatus)context);
 		break;
 	case eSAP_ACS_CHANNEL_SELECTED:
 		sap_ap_event.sapHddEventCode = sap_hddevent;
 		acs_selected = &sap_ap_event.sapevt.sap_ch_selected;
-		if (eSAP_STATUS_SUCCESS == (eSapStatus)(uintptr_t)context) {
+		if (eSAP_STATUS_SUCCESS == (eSapStatus)context) {
 			acs_selected->pri_ch_freq =
 						sap_ctx->acs_cfg->pri_ch_freq;
 			acs_selected->ht_sec_ch_freq =
@@ -1512,7 +1504,7 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 				sap_ctx->acs_cfg->vht_seg0_center_ch_freq;
 			acs_selected->vht_seg1_center_ch_freq =
 				sap_ctx->acs_cfg->vht_seg1_center_ch_freq;
-		} else if (eSAP_STATUS_FAILURE == (eSapStatus)(uintptr_t)context) {
+		} else if (eSAP_STATUS_FAILURE == (eSapStatus)context) {
 			acs_selected->pri_ch_freq = 0;
 		}
 		break;
@@ -1520,7 +1512,7 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 	case eSAP_STOP_BSS_EVENT:
 		sap_ap_event.sapHddEventCode = eSAP_STOP_BSS_EVENT;
 		sap_ap_event.sapevt.sapStopBssCompleteEvent.status =
-			(eSapStatus)(uintptr_t)context;;
+			(eSapStatus) context;
 		break;
 
 	case eSAP_STA_ASSOC_EVENT:
@@ -1594,7 +1586,7 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 		chaninfo->rate_flags = csr_roaminfo->chan_info.rate_flags;
 
 		reassoc_complete->wmmEnabled = csr_roaminfo->wmmEnabledSta;
-		reassoc_complete->status = (eSapStatus)(uintptr_t)context;
+		reassoc_complete->status = (eSapStatus) context;
 		reassoc_complete->timingMeasCap = csr_roaminfo->timingMeasCap;
 		reassoc_complete->ampdu = csr_roaminfo->ampdu;
 		reassoc_complete->sgi_enable = csr_roaminfo->sgi_enable;
@@ -1638,7 +1630,7 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 			disassoc_comp->reason = eSAP_MAC_INITATED_DISASSOC;
 
 		disassoc_comp->status_code = csr_roaminfo->status_code;
-		disassoc_comp->status = (eSapStatus)(uintptr_t)context;
+		disassoc_comp->status = (eSapStatus) context;
 		disassoc_comp->rssi = csr_roaminfo->rssi;
 		disassoc_comp->rx_rate = csr_roaminfo->rx_rate;
 		disassoc_comp->tx_rate = csr_roaminfo->tx_rate;
@@ -1657,7 +1649,7 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 		sap_ap_event.sapHddEventCode = eSAP_STA_SET_KEY_EVENT;
 		key_complete =
 			&sap_ap_event.sapevt.sapStationSetKeyCompleteEvent;
-		key_complete->status = (eSapStatus)(uintptr_t)context;
+		key_complete->status = (eSapStatus) context;
 		qdf_copy_macaddr(&key_complete->peerMacAddr,
 				 &csr_roaminfo->peerMac);
 		break;
@@ -1707,13 +1699,13 @@ QDF_STATUS sap_signal_hdd_event(struct sap_context *sap_ctx,
 	case eSAP_DISCONNECT_ALL_P2P_CLIENT:
 		sap_ap_event.sapHddEventCode = eSAP_DISCONNECT_ALL_P2P_CLIENT;
 		sap_ap_event.sapevt.sapActionCnf.actionSendSuccess =
-			(eSapStatus)(uintptr_t)context;;
+			(eSapStatus) context;
 		break;
 
 	case eSAP_MAC_TRIG_STOP_BSS_EVENT:
 		sap_ap_event.sapHddEventCode = eSAP_MAC_TRIG_STOP_BSS_EVENT;
 		sap_ap_event.sapevt.sapActionCnf.actionSendSuccess =
-			(eSapStatus)(uintptr_t)context;;
+			(eSapStatus) context;
 		break;
 
 	case eSAP_UNKNOWN_STA_JOIN:
