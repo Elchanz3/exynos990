9b73736b8fc8c334c2e7d283ad5b2aefdebbf7ae
Elchanz3 <inutilidades639@gmail.com>
2025-08-27 23:08:19 -0300

    mali : overclock to 921MHz

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1633,7 +1633,7 @@ static __init int cpufreq_read_cpu_max_c3(char *cpu_max_c3)
 }
 __setup("cpu_max_c3=", cpufreq_read_cpu_max_c3);
 
-unsigned long arg_gpu_max = 932000;
+unsigned long arg_gpu_max = 921000;
 
 static __init int cpufreq_read_gpu_max(char *gpu_max)
 {
